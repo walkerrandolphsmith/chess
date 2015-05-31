d3b021a654fe475af08980d410ad8b5205eec719
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-05-31 19:53:37 -0400

    Moved the reset template to be part of the aside template. Added events to aside tempalte to reset game.

--- client/aside/aside.js
+++ client/aside/aside.js
@@ -3,3 +3,13 @@ Template.Aside.helpers({
         return Settings.findOne({player: Meteor.userId()});
     }
 });
+
+Template.Aside.events({
+    "click .reset": function(){
+        var game = Games.findOne({players: Meteor.userId()});
+        Games.update(game._id, {$set: {
+            squares: Meteor.Game.generateSquares(),
+            currentPlayer: game.players[0]
+        }});
+    }
+});
--- client/reset-game/reset-game.js
+++
@@ -1,9 +0,0 @@
-Template.Reset.events({
-    "click": function(){
-        var game = Games.findOne({players: Meteor.userId()});
-        Games.update(game._id, {$set: {
-            squares: Meteor.Game.generateSquares(),
-            currentPlayer: game.players[0]
-        }});
-    }
-});
