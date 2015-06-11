d5ca67b66ec69c3f90809a200d992a759b6a099b
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2015-06-10 22:20:48 -0400

    Rolled custom selection to replace aside (cover by another element) with new cotent.

--- client/dropdown/dropdown.js
+++ client/dropdown/dropdown.js
@@ -4,3 +4,13 @@ Template.Dropdown.helpers({
         return JSON.parse(categories);
     }
 });
+
+Template.Dropdown.events({
+    "click": function(event){
+        var id = Template.currentData().id;
+        $("#" + id).toggleClass('active');
+    },
+    "click ul li": function(e){
+        var option = e.target.text;
+    }
+});
